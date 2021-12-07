e9f078eed8ded7dee09e37b36b071926c0ba90ce
anonymous anonymous <andrewkallick@gmail.com>
2021-12-07 16:49:07 +0000

    Whitelist columns in FavoritedBrand for ActiveAdmin

--- app/admin/favorited_brands.rb
+++ app/admin/favorited_brands.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register FavoritedBrand do
 
+ permit_params :brand_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
