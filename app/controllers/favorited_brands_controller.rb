89e1d138344ed26d60edfd83a342fefe39e4e8fb
anonymous anonymous <andrewkallick@gmail.com>
2021-12-07 16:49:15 +0000

    Display associated collections on show pages

--- app/controllers/favorited_brands_controller.rb
+++ app/controllers/favorited_brands_controller.rb
@@ -42,8 +42,14 @@ class FavoritedBrandsController < ApplicationController
   # DELETE /favorited_brands/1
   def destroy
     @favorited_brand.destroy
-    redirect_to favorited_brands_url, notice: 'Favorited brand was successfully destroyed.'
+    message = "FavoritedBrand was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to favorited_brands_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
